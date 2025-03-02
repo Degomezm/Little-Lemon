c025c02338855e36e246f6b90a67680e1f91e8b5
Daniel Gómez <92500098+Degomezm@users.noreply.github.com>
2025-03-02 16:00:52 -0500

    correct the path to the booking and the menu app

--- littlelemon/littlelemon/urls.py
+++ littlelemon/littlelemon/urls.py
@@ -25,8 +25,8 @@ router.register(r'booking', BookingViewSet, basename='booking')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('restaurant/menu/', include('restaurant.urls')),
-    path('restaurant/booking/', include(router.urls)),
+    path('restaurant/', include('restaurant.urls')),
+    path('restaurant/', include(router.urls)),
     path('auth/', include('djoser.urls')),
     path('auth-token/', include('djoser.urls.authtoken')),
     path('api-token-auth/', obtain_auth_token),
